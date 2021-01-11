ba0476086ba6bcc8ae72027a11042673a595c132
gabbayron <gabbayron2307@gmail.com>
2021-01-11 14:47:47 +0200

    added main component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { IntroComponent } from './components/intro/intro.component';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    IntroComponent
+    IntroComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
