368b892fd55a62b110ebf2dc4dbf04bb1872718f
gabbayron <gabbayron2307@gmail.com>
2021-01-13 15:25:08 +0200

    changed icons sections responsive

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -17,6 +17,9 @@ export class MainComponent implements OnInit {
   ngOnInit(): void {
 
     window.innerWidth > 1160 ? this.config.slidesPerView = 2 : this.config.slidesPerView = 1
+    window.innerWidth > 1160 ? null : this.config.spaceBetween = 15
+    window.innerWidth > 1160 ? null : this.config.width = 235
+
   }
 
 }
